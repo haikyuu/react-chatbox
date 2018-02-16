45ddc3905f560fedebb865efcb49c2424279bf0d
Abdellah Alaoui <als.abdellah@gmail.com>
2018-02-16 08:07:53 +0000

    Add ability to specify launcher icon in default & active state
    - fix a bug with failing svg import when the loader is not used in the
    user's app

--- src/lib/components/Launcher.js
+++ src/lib/components/Launcher.js
@@ -1,43 +1,65 @@
-import PropTypes from "prop-types";
-import React, { Component } from "react";
-import ChatWindow from "./ChatWindow";
-import launcherIcon from "./../assets/logo-no-bg.svg";
-import launcherIconActive from "./../assets/close-icon.png";
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import ChatWindow from './ChatWindow'
+import DefaultLauncherIcon from './icons/DefaultLauncherIcon'
+import defaultLauncherIconActive from './../assets/close-icon.png'
 
 class Launcher extends Component {
+  static propTypes = {
+    onMessageWasReceived: PropTypes.func,
+    onMessageWasSent: PropTypes.func,
+    newMessagesCount: PropTypes.number,
+    isOpen: PropTypes.bool,
+    handleClick: PropTypes.func,
+    messageList: PropTypes.arrayOf(PropTypes.object),
+    showEmoji: PropTypes.bool,
+    launcherIcon: PropTypes.string,
+    launcherIconActive: PropTypes.string,
+  }
   constructor() {
-    super();
+    super()
     this.state = {
-      launcherIcon,
-      isOpen: false
-    };
+      isOpen: false,
+    }
   }
 
   handleClick() {
     if (this.props.handleClick !== undefined) {
-      this.props.handleClick();
+      this.props.handleClick()
     } else {
       this.setState({
-        isOpen: !this.state.isOpen
-      });
+        isOpen: !this.state.isOpen,
+      })
     }
   }
 
   render() {
-    const isOpen = this.props.hasOwnProperty("isOpen")
+    const isOpen = this.props.hasOwnProperty('isOpen')
       ? this.props.isOpen
-      : this.state.isOpen;
-    const classList = ["sc-launcher", isOpen ? "opened" : ""];
+      : this.state.isOpen
+    const classList = ['sc-launcher', isOpen ? 'opened' : '']
     return (
       <div>
         <div />
         <div
-          className={classList.join(" ")}
+          className={classList.join(' ')}
           onClick={this.handleClick.bind(this)}
         >
           <MessageCount count={this.props.newMessagesCount} isOpen={isOpen} />
-          <img alt="open" className={"sc-open-icon"} src={launcherIconActive} />
-          <img alt="close" className={"sc-closed-icon"} src={launcherIcon} />
+          <img
+            alt="open"
+            className="sc-open-icon"
+            src={this.props.launcherIconActive}
+          />
+          {this.props.launcherIcon ? (
+            <img
+              alt="close"
+              className="sc-closed-icon"
+              src={this.props.launcherIcon}
+            />
+          ) : (
+            <DefaultLauncherIcon className="sc-closed-icon" />
+          )}
         </div>
         <ChatWindow
           messageList={this.props.messageList}
@@ -48,30 +70,21 @@ class Launcher extends Component {
           showEmoji={this.props.showEmoji}
         />
       </div>
-    );
+    )
   }
 }
 
 const MessageCount = props => {
   if (props.count === 0 || props.isOpen === true) {
-    return null;
+    return null
   }
-  return <div className={"sc-new-messsages-count"}>{props.count}</div>;
-};
-
-Launcher.propTypes = {
-  onMessageWasReceived: PropTypes.func,
-  onMessageWasSent: PropTypes.func,
-  newMessagesCount: PropTypes.number,
-  isOpen: PropTypes.bool,
-  handleClick: PropTypes.func,
-  messageList: PropTypes.arrayOf(PropTypes.object),
-  showEmoji: PropTypes.bool
-};
+  return <div className="sc-new-messsages-count">{props.count}</div>
+}
 
 Launcher.defaultProps = {
   newMessagesCount: 0,
-  showEmoji: true
-};
+  showEmoji: true,
+  launcherIconActive: defaultLauncherIconActive,
+}
 
-export default Launcher;
+export default Launcher
